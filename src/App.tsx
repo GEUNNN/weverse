b9519722c4e2c6320abc37ba707b36600f8e8aaf
Geunhwa Lee <geunhwa.lee.01@gmail.com>
2022-12-17 22:23:18 +0900

    chore: add styling and routing package

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello world!
+      <img
+        src="https://ssl.pstatic.net/static/wevweb/assets/web/home_banner_slogan.gif"
+        alt="mid banner"
+      />
     </div>
   );
 }
